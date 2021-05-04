b0096505e1ee6d18d4bcac355c1ca2a7ff08b3ae
open-schnick <dev2@filefighter.de>
2021-05-04 11:50:32 +0200

    Added test.

--- src/de/schnick/open/Rental.java
+++ src/de/schnick/open/Rental.java
@@ -1,15 +1,18 @@
 package de.schnick.open;
 
-class Rental {
+public class Rental {
     private Movie movie;
     private int daysRented;
+
     public Rental(Movie newmovie, int newdaysRented) {
         movie = newmovie;
         daysRented = newdaysRented;
     }
+
     public int getDaysRented() {
         return daysRented;
     }
+
     public Movie getMovie() {
         return movie;
     }
